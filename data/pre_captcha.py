5256907729556543834f3cac21d92830c636235a
litterzhang <litterzhang824@gmail.com>
2016-07-19 12:24:57 +0800

    更改验证码预处理算法

--- data/pre_captcha.py
+++ data/pre_captcha.py
@@ -10,40 +10,30 @@ from PIL import Image, ImageEnhance, ImageFilter
 
 # 处理图片以便后续的0-1二值化
 def Handle_Image(im):
-    im = im.convert('L')
+    im = im.convert("RGB")
 
     for j in range(im.size[1]):
         for i in range(im.size[0]):
-            #Gray = Change_Gray(im.getpixel((i, j)))  # 灰度化
-            #im.putpixel([i, j], (Gray, Gray, Gray))
             if i == 0 or i == (im.size[0] - 1):  # 将图片的第一行和最后一行设为白色。
-                im.putpixel([i, j], 255)
+                im.putpixel([i, j], (255, 255, 255))
             if j == 0 or j == (im.size[1] - 1):  # 将图片的第一列和最后一列设为白色。
-                im.putpixel([i, j], 255)
-    enhancer = ImageEnhance.Contrast(im)  # 增加对比对
-    im = enhancer.enhance(2)
-    enhancer = ImageEnhance.Sharpness(im)  # 锐化
-    im = enhancer.enhance(2)
-    enhancer = ImageEnhance.Brightness(im)  # 增加亮度
-    im = enhancer.enhance(2)
-    im = im.filter(ImageFilter.DETAIL) #滤镜效果
+                im.putpixel([i, j], (255, 255, 255))
+
+            color = im.getpixel((i, j))
+            if sum(color)<450:
+                im.putpixel([i, j], (0, 0, 0))
+            else:
+                im.putpixel([i, j], (255, 255, 255))
 
     im = im.convert("1")
 
     im = Clear_Point(im)  # 清除周围8个像素都是白色的孤立噪点
     im = Clear_Point_Twice(im)  # 清除两个孤立的噪点：周围8个像素中有7个是白色，而唯一的黑色像素对应的他的邻域（他周围的8个像素）中唯一的黑色像素是自身。
-    im = Clear_Point_Third(im)  # 清除第三种噪点：左右都是3个（含）以上的空白列，自身相邻的3个列上的X值投影不大于3.
 
     im = Clear_line(im)
     im = Clear_Point(im)
     im = Clear_Point_Twice(im)
-    im = Clear_Point_Third(im) 
     return im
-
-# 改变灰度，查文献后发现据说按照下面的R，G，B数值的比例进行调整，图像的灰度最合适。
-def Change_Gray(RGB_Value):
-    Gray = int((RGB_Value[0] * 299 + RGB_Value[1] * 587 + RGB_Value[2] * 114) / 1000)
-    return Gray
 
 # 初步清除干扰线
 def Clear_line(im):
@@ -131,58 +121,6 @@ def Caculate_X(im):
         Image_Value.append(Y_pixel)
     return Image_Value
 
-def Set_White_Y(im, List_Black):
-    for j in range(im.size[1]):
-        for i in range(List_Black[0], (List_Black[(len(List_Black) - 1)])):
-            im.putpixel([i, j], 255)
-    return im
-
-# 清除第三种残余的孤立点
-def Clear_Point_Third(im):
-    Image_Value = Caculate_X(im)
-    List01 = []
-    List_Black = []
-    List03 = []
-    for i in range(len(Image_Value)):  # 从左到右扫描
-        if Image_Value[i] == 0 and len(List_Black) == 0:  # X轴投影是0，说明是空白列，黑色列的列表是空值，说明当前列是黑色列的左侧
-            List01.append(i)
-        elif Image_Value[i] > 0:  # X周投影大于0的列，即扫描到了黑色列
-            List_Black.append(i)
-        elif Image_Value[i] == 0 and len(List_Black) > 0 and len(
-                List_Black) <= 3:  # 黑色列的列表的长度大于0，不大于3个空白字符，现在的X轴投影为0，说明现在扫描到了孤立噪点所在的黑色列右侧的空白列
-            List03.append(i)
-            if len(List03) == 3:  # 空白列为3列
-                im = Set_White_Y(im, List_Black)  # 逐次将多列设为全白
-                List01 = []
-                List_Black = []
-                List03 = []
-        elif Image_Value[i] == 0 and len(List_Black) > 3:  # 当前是空白列，黑色列的数量大于3，说明扫描到了数字所在部分（不是噪点）的右侧空白列。
-            List01 = []
-            List_Black = []
-            List03 = []
-            List01.append(i)
-    return im
-
-# 返回矩阵各行最大值位置的函数，以便找到有颜色的列中X轴投影最大的地方
-def Max_Index(List1):
-    Max = 0
-    Max_index = 0
-    for i in range(len(List1)):
-        if len(List1[i]) > Max:
-            Max = len(List1[i])
-            Max_index = i
-    return Max_index
-
-# 返回矩阵各行最小值位置的函数，以便找到有颜色的列中X轴投影最小的地方
-def Min_Index(List1):
-    Min = 50
-    Min_index = 0
-    for i in range(len(List1)):
-        if len(List1[i]) < Min:
-            Min = len(List1[i])
-            Min_index = i
-    return Min_index
-
 # x轴投影
 def Get_X(Image_Value):
     X_Value = []
@@ -212,6 +150,7 @@ def handle(loaddirpath, savedirpath):
         im = Handle_Image(im)
 
         im.save(os.path.join(savedirpath, 'pre_%s' % filename))
+        # break
 
 if __name__=='__main__':
     handle(os.path.join(os.path.dirname(__file__), 'img'), \
